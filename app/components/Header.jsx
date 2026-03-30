3935207074444bc3e514fb3b3f092b7f04271e9b
Sanketmaity <maitysanket269@gmail.com>
2026-03-30 11:36:25 +0530

    Change resume download link in Header.jsx
    Updated the resume link in the Header component.

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -36,7 +36,7 @@ const Header = () => {
                 <Image src={assets.right_arrow_white} alt='' className='w-4 transition-transform 
                 duration-200 group-hover:translate-x-1'/>
             </a>
-            <a href='/sample-resume.pdf' download 
+            <a href='/bwu_bta_23_158_Sanket_Maity-08.pdf' download 
             className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 
             transition-transform duration-200 hover:scale-105 hover:border-black dark:text-black dark:bg-white'>
                 Resume
